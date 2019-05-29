64e83f058c37b0a8a6d8f6b682847a61348e52b8
Sarah Chima <sarahchimao@gmail.com>
2019-05-29 23:23:13 +0100

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-NoteApp is a note-taking app built with React and uses local storage to save notes.
-
-Some Features of it are:
-1. Add new note
-2. Delete note
-3. Edit Note
-4. Delete all note
-5. Dark or Light mode
---
+++ precache-manifest.afaf3c5880d324c41bf3c5ac52f84184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f0f08c62956ceaa4d31d373fa53b54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687dd2803ba6c23a4d8a",
+    "url": "/static/css/main.f39c708e.chunk.css"
+  },
+  {
+    "revision": "faa34d332425c18142f5",
+    "url": "/static/js/2.05fa6133.chunk.js"
+  },
+  {
+    "revision": "687dd2803ba6c23a4d8a",
+    "url": "/static/js/main.c71228dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
