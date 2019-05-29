7f6768c87d10767b4d9e5b6761a2708ae81d02b5
Sarah Chima <sarahchimao@gmail.com>
2019-05-29 22:47:54 +0100

    Updates

---
+++ precache-manifest.78ae0aca6033c08e55552cabb4a7ad14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a4c952edb1132c43101945f86ddb85",
+    "url": "/NoteApp/index.html"
+  },
+  {
+    "revision": "3d46bf0ebf66c1f47ba8",
+    "url": "/NoteApp/static/css/main.f39c708e.chunk.css"
+  },
+  {
+    "revision": "faa34d332425c18142f5",
+    "url": "/NoteApp/static/js/2.05fa6133.chunk.js"
+  },
+  {
+    "revision": "3d46bf0ebf66c1f47ba8",
+    "url": "/NoteApp/static/js/main.eb04cb1c.chunk.js"
+  },
+  {
+    "revision": "bf0f1a2bf962c5ce474d",
+    "url": "/NoteApp/static/js/runtime~main.e21af594.js"
+  }
+]);
